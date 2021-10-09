bb600287dc710df1721721f63d596b92731056e5
장유진(무은재학부) <jangyj2020@postech.ac.kr>
2021-10-10 01:34:38 +0900

    make MainView

---
+++ src/components/Bigbutton/index.js
@@ -0,0 +1,5 @@
+import './index.css';
+
+export default function BigButton(props) {
+    return <button className='btn-3'>{props.name}</button>
+}
--- src/view/Main/index.js
+++ src/view/Main/index.js
@@ -1,7 +1,24 @@
-import Example from "../../components/Example";
+import { Link } from 'react-router-dom'
+import Button from '../../components/Bigbutton'
 
 export default function MainView() {
     return <div>
-        <Example />
+        <div className='container'>
+            <div className='left'>
+                <Link to='../Review/Create'>
+                    <button>리뷰 작성하기</button>
+                </Link>
+            </div>
+            <div className='mid'>
+                <Link to='../Near'>
+                    <button>주변 맛집 찾기</button>
+                </Link>
+            </div>
+            <div className='right'>
+                <Link to='../Search'>
+                    <button>식당 검색</button>
+                </Link>
+            </div>
+        </div>
     </div>
 }
