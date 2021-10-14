f166c7ad130db9d915e5b770aed5fe22e13344be
장유진(무은재학부) <jangyj2020@postech.ac.kr>
2021-10-14 23:29:55 +0900

    bigbutton css complete

--- src/view/Main/index.js
+++ src/view/Main/index.js
@@ -1,22 +1,22 @@
 import { Link } from 'react-router-dom'
-import Button from '../../components/Bigbutton'
+import BigButton from '../../components/Bigbutton'
 
 export default function MainView() {
     return <div>
         <div className='container'>
             <div className='left'>
                 <Link to='../Review/Create'>
-                    <button>리뷰 작성하기</button>
+                    <BigButton name='리뷰 작성하기' />
                 </Link>
             </div>
             <div className='mid'>
                 <Link to='../Near'>
-                    <button>주변 맛집 찾기</button>
+                    <BigButton name='주변 맛집 찾기' />
                 </Link>
             </div>
             <div className='right'>
                 <Link to='../Search'>
-                    <button>식당 검색</button>
+                    <BigButton name='식당 검색' />
                 </Link>
             </div>
         </div>
