56d150d5a9e968ac78997159cff2470f6a881a2f
dmsgk724 <dmsgk724@postech.ac.kr>
2021-11-02 00:01:17 +0900

    mypage

---
+++ src/view/Review/Create/readMultipleImage.js
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from "react-bootstrap";
+import React, { Components } from 'React';
+
+// class readMultipleImage extends Components {
+
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+
+//         }
+//     }
+
+//     render() {
+
+//     }
+// }
