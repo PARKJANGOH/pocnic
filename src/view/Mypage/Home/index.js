ad5e8c7017e0233b02052f10d55ee133d8f7850d
dmsgk724 <dmsgk724@postech.ac.kr>
2021-11-07 16:41:42 +0900

    like

--- src/view/Mypage/Home/index.js
+++ src/view/Mypage/Home/index.js
@@ -16,163 +16,92 @@ export default function MypageHomeView() {
   //export default function MypageHomeView() {
   //  return <List />;
   //}
-
-  const App = () => (
-    <Image.PreviewGroup>
-      <Image width={200} height={230} src={YJ} />
-      <Image width={200} height={230} src={SY} />
-      <Image width={200} height={230} src={EH} />
-    </Image.PreviewGroup>
-  );
+  let app;
   if (write === 1) {
-    return (
-      <Layout>
-        <Header className="header">
-          <div className="logo" />
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={["2"]}
-            height={100}
-          >
-            <div>This page is for Ohjangpark.</div>
-          </Menu>
-        </Header>
-        <Content style={{ padding: "0 50px" }}>
-          <Breadcrumb style={{ margin: "16px 0" }}>
-            <Breadcrumb.Item>MYPAGE</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-          </Breadcrumb>
-          <Layout
-            className="site-layout-background"
-            style={{ padding: "24px 0" }}
-          >
-            <Sider className="site-layout-background" width={200}>
-              <Menu
-                mode="inline"
-                defaultSelectedKeys={["1"]}
-                defaultOpenKeys={["sub1"]}
-                style={{ height: "100%" }}
-              >
-                <SubMenu
-                  key="sub1"
-                  onClick={() => setWrite(1)}
-                  icon={<UserOutlined />}
-                  title="USER-INFO"
-                >
-                  <Menu.Item key="1" onClick={() => setWrite(1)}>
-                    {" "}
-                    PROFILE{" "}
-                  </Menu.Item>
-                  <Menu.Item key="2" onClick={() => setWrite(3)}>
-                    LEVEL
-                  </Menu.Item>
-                </SubMenu>
-                <SubMenu
-                  key="sub2"
-                  onClick={() => setWrite(2)}
-                  icon={<UserOutlined />}
-                  title="REVIEW"
-                >
-                  <Menu.Item key="3" onClick={() => setWrite(2)}>
-                    WRITTENBY
-                  </Menu.Item>
-                  <Menu.Item key="4" onClick={() => setWrite(3)}>
-                    LIKE
-                  </Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub3" icon={<LaptopOutlined />} title="SETTING">
-                  <Link to="mypage/setting">
-                    <Menu.Item key="5">SETTING</Menu.Item>
-                  </Link>
-                </SubMenu>
-              </Menu>
-            </Sider>
-
-            <Content style={{ padding: "0 24px", minHeight: 280 }}>
-              <App />
-            </Content>
-          </Layout>
-        </Content>
-        <Footer style={{ textAlign: "center" }}>
-          THIS IS FOR POCNIC @ ohjangpark.github.com
-        </Footer>
-      </Layout>
+    app = (
+      //서버에서 사진 받아오기
+      <Image.PreviewGroup>
+        <Image width={200} height={230} src={YJ} />
+        <Image width={200} height={230} src={SY} />
+        <Image width={200} height={230} src={EH} />
+      </Image.PreviewGroup>
     );
   } else if (write === 2) {
-    return (
-      <Layout>
-        <Header className="header">
-          <div className="logo" />
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={["2"]}
-            height={100}
-          >
-            <div>THIS IS PAGE FOR OHJANGPARK!!</div>
-          </Menu>
-        </Header>
-        <Content style={{ padding: "0 50px" }}>
-          <Breadcrumb style={{ margin: "16px 0" }}>
-            <Breadcrumb.Item>MYPAGE</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-          </Breadcrumb>
-          <Layout
-            className="site-layout-background"
-            style={{ padding: "24px 0" }}
-          >
-            <Sider className="site-layout-background" width={200}>
-              <Menu
-                mode="inline"
-                defaultSelectedKeys={["1"]}
-                defaultOpenKeys={["sub1"]}
-                style={{ height: "100%" }}
-              >
-                <SubMenu
-                  key="sub1"
-                  onClick={() => setWrite(1)}
-                  icon={<UserOutlined />}
-                  title="USER-INFO"
-                >
-                  <Menu.Item key="1" onClick={() => setWrite(1)}>
-                    {" "}
-                    PROFILE{" "}
-                  </Menu.Item>
-                  <Menu.Item key="2" onClick={() => setWrite(1)}>
-                    LEVEL
-                  </Menu.Item>
-                </SubMenu>
-                <SubMenu
-                  key="sub2"
-                  onClick={() => setWrite(2)}
-                  icon={<UserOutlined />}
-                  title="REVIEW"
-                >
-                  <Menu.Item key="3" onClick={() => setWrite(2)}>
-                    WRITTENBY
-                  </Menu.Item>
-                  <Menu.Item key="4" onClick={() => setWrite(3)}>
-                    LIKE
-                  </Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub3" icon={<LaptopOutlined />} title="SETTING">
-                  <Link to="WHAT?">
-                    <Menu.Item key="5">SETTING</Menu.Item>
-                  </Link>
-                </SubMenu>
-              </Menu>
-            </Sider>
-
-            <Content style={{ padding: "0 24px", minHeight: 280 }}>
-              <List />
-            </Content>
-          </Layout>
-        </Content>
-        <Footer style={{ textAlign: "center" }}>
-          THIS IS FOR POCNIC @ ohjangpark.github.com
-        </Footer>
-      </Layout>
-    );
+    app = <List />;
+    //서버에서 list만들어 오기
+  } else if (write === 3 || write === 4) {
+    app = <List />;
   }
+  return (
+    <Layout>
+      <Header className="header">
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["2"]}
+          height={100}
+        >
+          <div>This page is for Ohjangpark.</div>
+        </Menu>
+      </Header>
+      <Content style={{ padding: "0 50px" }}>
+        <Breadcrumb style={{ margin: "16px 0" }}>
+          <Breadcrumb.Item>MYPAGE</Breadcrumb.Item>
+          <Breadcrumb.Item>PARK JANG OH</Breadcrumb.Item>
+        </Breadcrumb>
+        <Layout
+          className="site-layout-background"
+          style={{ padding: "24px 0" }}
+        >
+          <Sider className="site-layout-background" width={300}>
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={["1"]}
+              defaultOpenKeys={["sub1"]}
+              style={{ height: "100%" }}
+            >
+              <SubMenu
+                key="sub1"
+                onClick={() => setWrite(1)}
+                icon={<UserOutlined />}
+                title="USER-INFO"
+              >
+                <Menu.Item key="1" onClick={() => setWrite(1)}>
+                  {" "}
+                  PROFILE{" "}
+                </Menu.Item>
+                <Menu.Item key="2" onClick={() => setWrite(2)}>
+                  LEVEL
+                </Menu.Item>
+              </SubMenu>
+              <SubMenu
+                key="sub2"
+                onClick={() => setWrite(2)}
+                icon={<UserOutlined />}
+                title="REVIEW"
+              >
+                <Menu.Item key="3" onClick={() => setWrite(3)}>
+                  WRITTENBY
+                </Menu.Item>
+                <Menu.Item key="4" onClick={() => setWrite(4)}>
+                  LIKE
+                </Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub3" icon={<LaptopOutlined />} title="SETTING">
+                <Link to="mypage/setting">
+                  <Menu.Item key="5">SETTING</Menu.Item>
+                </Link>
+              </SubMenu>
+            </Menu>
+          </Sider>
+
+          <Content style={{ padding: "0 24px", minHeight: 280 }}>{app}</Content>
+        </Layout>
+      </Content>
+      <Footer style={{ textAlign: "center" }}>
+        THIS IS FOR POCNIC @ ohjangpark.github.com
+      </Footer>
+    </Layout>
+  );
 }
