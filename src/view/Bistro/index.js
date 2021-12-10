5d406d3c5887453d697a8339e4cccda1740ea0f5
dmsgk724 <dmsgk724@postech.ac.kr>
2021-12-11 02:49:40 +0900

    mypage modify

--- src/view/Bistro/index.js
+++ src/view/Bistro/index.js
@@ -11,98 +11,96 @@ import axios from "axios";
 //import axios from "axios";
 
 class BistroView extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: [],
-            //   page: 1,
-            //   limit: 10,
-            //   all_page: [],
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      //   page: 1,
+      //   limit: 10,
+      //   all_page: [],
+    };
+  }
 
-    componentWillMount() {
-        this._getListData();
-        //this._setPage();
-    }
+  componentWillMount() {
+    this._getListData();
+    //this._setPage();
+  }
 
-    _getListData = async function () {
+  _getListData = async function () {
+    //데이터 가져오기
+    const total_list = await axios.get(
+      `http://localhost:4000/review/reviewOf/2`
+    );
+    console.log(total_list);
+    this.setState({ data: total_list.data });
+  };
 
-        //데이터 가져오기
-        const total_list = await axios.get(`http://localhost:4000/review/reviewOf/2`);
-        console.log(total_list);
-        this.setState({ data: total_list.data })
-    }
+  //   _changePage = function(el) {
+  //     this.setState({ page : el })
+  //     sessionStorage.setItem('page', el);
 
+  //     return this._getListData();
+  //   }
 
+  //   _setPage = function() {
+  //     if(sessionStorage.page) {
+  //       this.setState({ page : Number(sessionStorage.page) })
+  //       return Number(sessionStorage.page);
+  //     }
 
-    //   _changePage = function(el) {
-    //     this.setState({ page : el })
-    //     sessionStorage.setItem('page', el);
+  //     this.setState({ page : 1 })
+  //     return 1;
+  //   }
+  //   handleLike(num, content) {
+  //     const { data } = this.state;
 
-    //     return this._getListData();
-    //   }
+  //     this.setState({
+  //       data: data.map(
+  //         (info) =>
+  //           num === info.id
+  //             ? { ...info, like: 1 } // 새 객체를 만들어서 기존의 값과 전달받은 data 을 덮어씀
+  //             : info // 기존의 값을 그대로 유지
+  //       ),
+  //     });
+  //   }
 
-    //   _setPage = function() {
-    //     if(sessionStorage.page) {
-    //       this.setState({ page : Number(sessionStorage.page) })
-    //       return Number(sessionStorage.page);
-    //     }
+  render() {
+    const list = this.state.data;
+    // const { all_page, page } = this.state;
 
-    //     this.setState({ page : 1 })
-    //     return 1;
-    //   }
-    //   handleLike(num, content) {
-    //     const { data } = this.state;
-
-    //     this.setState({
-    //       data: data.map(
-    //         (info) =>
-    //           num === info.id
-    //             ? { ...info, like: 1 } // 새 객체를 만들어서 기존의 값과 전달받은 data 을 덮어씀
-    //             : info // 기존의 값을 그대로 유지
-    //       ),
-    //     });
-    //   }
-
-    render() {
-
-        const list = this.state.data;
-        // const { all_page, page } = this.state;
-        const data = this.state.data;
-        return (
-            <div className="List">
-                <div className="list_grid list_tit">
-                    <div> 제목 </div>
-                    <div> 조회수 </div>
-                    <div className="acenter"> 날짜 </div>
-                </div>
-                {list
-                    ? list.map((el, key) => {
-                        const view_url = "/view/" + el.id;
-                        return (
-                            <>
-                                <div className="list_grid list_data" key={key}>
-                                    <div>
-                                        {" "}
-                                        <Link to={view_url}> {el.title} </Link>{" "}
-                                    </div>
-                                    <div> </div>
-                                    <div className="acenter"> {el.rating} </div>
-                                    {/* <Image.PreviewGroup>
+    return (
+      <div className="List">
+        <div className="list_grid list_tit">
+          <div> 제목 </div>
+          <div> 조회수 </div>
+          <div className="acenter"> 날짜 </div>
+        </div>
+        {list
+          ? list.map((el, key) => {
+              const view_url = "/view/" + el.id;
+              return (
+                <>
+                  <div className="list_grid list_data" key={key}>
+                    <div>
+                      {" "}
+                      <Link to={view_url}> {el.title} </Link>{" "}
+                    </div>
+                    <div> </div>
+                    <div className="acenter"> {el.rating} </div>
+                    {/* <Image.PreviewGroup>
                                         <Image width={400} height={500} src={el.img} />
                                     </Image.PreviewGroup> */}
-                                </div>
+                  </div>
 
-                                <div>{el.content}</div>
-                                <div className="line" />
-                            </>
-                        );
-                    })
-                    : null}
-            </div>
-        );
-    }
+                  <div>{el.content}</div>
+                  <div className="line" />
+                </>
+              );
+            })
+          : null}
+      </div>
+    );
+  }
 }
 
 export default BistroView;
