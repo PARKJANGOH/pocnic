54a7da333328acc4b27a199c2a706300ddca4681
dmsgk724 <dmsgk724@postech.ac.kr>
2021-12-11 03:34:25 +0900

    15:34

--- src/view/Main/index.js
+++ src/view/Main/index.js
@@ -1,24 +1,32 @@
-import { Link } from 'react-router-dom'
-import BigButton from '../../components/Bigbutton'
+import { Link } from "react-router-dom";
+import BigButton from "../../components/Bigbutton";
+import { Image } from "antd";
+import logo from "../../OCNIC.png";
 
 export default function MainView() {
-  return <div>
-    <div className='container'>
-      <div className='left'>
-        <Link to='../Review/Create'>
-          <BigButton name='리뷰 작성하기' />
-        </Link>
-      </div>
-      <div className='mid'>
-        <Link to='../Near'>
-          <BigButton name='주변 맛집 찾기' />
-        </Link>
-      </div>
-      <div className='right'>
-        <Link to='../Search'>
-          <BigButton name='식당 검색' />
-        </Link>
-      </div>
+  return (
+    <div>
+      <center>
+        <Image src={logo} />
+
+        <div className="container">
+          <div className="left">
+            <Link to="../Review/Create">
+              <BigButton name="리뷰 작성하기" />
+            </Link>
+          </div>
+          <div className="mid">
+            <Link to="../Near">
+              <BigButton name="주변 맛집 찾기" />
+            </Link>
+          </div>
+          <div className="right">
+            <Link to="../Search">
+              <BigButton name="식당 검색" />
+            </Link>
+          </div>
+        </div>
+      </center>
     </div>
-  </div>
+  );
 }
